46ac828bd7cb28d6e84a50b822bd02e0cf74e978
Matthew Murray <admin@matdragon.com>
2020-08-20 01:49:21 +0100

    added file model

--- apps/site/models/file.php
+++ apps/site/models/file.php
@@ -13,16 +13,23 @@ class file_model extends site_model
 		$this->set_pkey( 'fid' );
 	}
 
+	public function get_basename()
+	{
+		return $this->get_prop( 'filename' ) . '.' . $this->get_prop( 'extension' );
+	}
+
 	public function set_default_fields()
 	{
 		$this->default_fields = [
-			'fid'        => $this->get_pkey_args(),
-			'created'    => [ 'type' => 'int', 'length' => 11 ],
-			'modified'   => [ 'type' => 'int', 'length' => 11 ],
-			'removed'    => [ 'type' => 'tinyint', 'length' => 1 ],
-			'alt'        => [ 'type' => 'varchar', 'length' => 255 ],
-			'path'       => [ 'type' => 'varchar', 'length' => 255 ],
-			'path_thumb' => [ 'type' => 'varchar', 'length' => 255 ],
+			'fid'       => $this->get_pkey_args(),
+			'created'   => [ 'type' => 'int', 'length' => 11 ],
+			'modified'  => [ 'type' => 'int', 'length' => 11 ],
+			'removed'   => [ 'type' => 'tinyint', 'length' => 1 ],
+			'alt'       => [ 'type' => 'varchar', 'length' => 255 ],
+			'path'      => [ 'type' => 'varchar', 'length' => 255 ],
+			'filename'  => [ 'type' => 'varchar', 'length' => 255 ],
+			'extension' => [ 'type' => 'varchar', 'length' => 255 ],
+			'source'    => [ 'type' => 'varchar', 'length' => 255 ],
 		];
 	}
 }
