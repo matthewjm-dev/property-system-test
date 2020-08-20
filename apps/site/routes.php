ef8cb821d846d2348189d224ed1594a31079a25f
Matthew Murray <admin@matdragon.com>
2020-08-20 01:50:31 +0100

    routing for site and admin

--- apps/site/routes.php
+++ apps/site/routes.php
@@ -1,13 +1,12 @@
 <?php // Site Static Routes
 
+$this->add_route_group('admin', false, function() {
+	$this->add_route( 'create', 'admin', 'property' );
+	$this->add_route( 'edit/*', 'admin', 'property' );
+	$this->add_route( 'process', 'admin', 'process' );
+	$this->add_route( 'remove/*', 'admin', 'remove' );
+	$this->add_route( '.', 'admin', 'index' );
+});
+
 $this->add_route( 'property', 'properties', 'property', ipsCore::$uri_parts );
 $this->add_route( '*', 'properties', 'index', ipsCore::$uri_parts );
-
-$this->add_route_group('admin', false, function() {
-	$this->add_route( 'properties', 'properties', 'index' );
-	$this->add_route( 'property/create', 'properties', 'create' );
-	$this->add_route( 'property/edit', 'properties', 'edit' );
-	$this->add_route( 'property/process', 'properties', 'process' );
-	$this->add_route( 'property/remove', 'properties', 'remove' );
-	$this->add_route( '.', 'dashboard', 'index' );
-});
